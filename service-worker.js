95b40507e64fa58167ba8163d24f228aebda80f5
Łukasz Jenczmyk (via Travis CI) <lukix.95@gmail.com>
2020-08-21 18:57:19 +0000

    Deploy lukix/creatures to github.com/lukix/creatures.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.599a98d86f9161c08a0700e4a5475ca9.js"
+  "creatures/precache-manifest.570ccd61ce5a0ce34afaf22223da50d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("creatures/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
